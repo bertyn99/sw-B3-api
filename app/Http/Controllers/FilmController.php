d92b7f7c3023e8af930d1a07c0875056ea4d0570
Damien Landois <landois.damien@live.fr>
2021-10-06 16:43:54 +0200

    Create FilmController.php

---
+++ app/Http/Controllers/FilmController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function index() {
+    //liste de tous les élements
+     return response()->json(Film::all());
+   }
+   public function show($id) {
+      $film = Film::find($id);
+      return response()->json($film,200);
+   }
+}
