6ad90b814b586ac30d5ade18a293805abc02460f
AlanAudran <65293707+AlanAudran@users.noreply.github.com>
2021-10-06 11:17:11 +0200

    ajout migration peoples_vehicles species_films

---
+++ database/migrations/2021_10_06_083758_create_peoples_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeoplesVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peoples_vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pilot');
+            $table->foreign('pilots')->references('id')->on('peoples');
+            $table->unsignedBigInteger('vehicle');
+            $table->foreign('vehicles')->references('id')->on('vehicles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peoples_vehicles');
+    }
+}
