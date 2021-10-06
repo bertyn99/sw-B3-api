b9a5b5debb4d70955365326c0ddab6462ca3b649
AlanAudran <65293707+AlanAudran@users.noreply.github.com>
2021-10-06 14:35:40 +0200

    PlanetController VehicleController

---
+++ app/Http/Controllers/PlanetController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlanetController extends Controller
+{
+    public function index() {
+        //liste de tous les élements
+             return response()->json(Planet::all());
+           }
+
+           public function store(Request $request) {
+             echo 'store'
+            }
+           }
+           public function show($id) {
+              $exemple = Planet::find($id);
+              return response()->json($exemple,200);
+           }
+           public function edit($id) {
+              echo 'edit';
+           }
+           public function update(Request $request, $id) {
+                echo 'update';
+           }
+           public function destroy($id) {
+              echo 'destroy';
+           }
+        }
+}
---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    public function index()
+    {
+        //liste de tous les élements
+       return response()->json(Vehicle::all());
+    }
+
+    public function store(Request $request)
+    {
+             echo 'store'
+    }
+
+    public function show($id)
+    {
+              $exemple = Vehicle::find($id);
+              return response()->json($exemple,200);
+    }
+    public function edit($id)
+    {
+              echo 'edit';
+    }
+    public function update(Request $request, $id)
+    {
+                echo 'update';
+    }
+    public function destroy($id)
+    {
+              echo 'destroy';
+    }
+
+}
