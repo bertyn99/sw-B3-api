8e997a72aef8811e969600b46c7a2aedda82e428
Damien Landois <landois.damien@live.fr>
2021-10-18 11:24:38 +0200

    Update ServicesDB.php

--- app/Services/ServicesDB.php
+++ app/Services/ServicesDB.php
@@ -4,6 +4,12 @@ namespace App\Services ;
 
 use GuzzleHttp\Client ; 
 use Illuminate\Support\Facades\Http;
+use App\Models\Film;
+use App\Models\People;
+use App\Models\Planet;
+use App\Models\Specie;
+use App\Models\Starship;
+use App\Models\Vehicle;
 
 class ServicesDB 
 {
