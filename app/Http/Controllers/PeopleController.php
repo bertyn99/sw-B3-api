5fce4a521b68506b4453edf6de5f3fc03293a7f1
Bertyn Boulikou <bboulikou@gmail.com>
2021-10-18 08:20:00 +0200

    ajout person id

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -7,62 +7,109 @@ use App\Models\People;
 
 class PeopleController extends Controller
 {
-     /**
-     * @OA\Get(
-     *      path="/people",
-     *      operationId="getAllPeople",
-     *      tags={"Tests"},
+  /**
+   * @OA\Get(
+   *      path="/people",
+   *      operationId="getAllPeople",
+   *      tags={"Tests"},
 
-     *      summary="Get List Of People",
-     *      description="Returns all people and associated vehicle, specie.",
-     *      @OA\Response(
-     *          response=200,
-     *          description="Successful operation",
-     *          @OA\MediaType(
-     *           mediaType="application/json",
-     *      )
-     *      ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Unauthenticated",
-     *      ),
-     *      @OA\Response(
-     *          response=403,
-     *          description="Forbidden"
-     *      ),
-     * @OA\Response(
-     *      response=400,
-     *      description="Bad Request"
-     *   ),
-     * @OA\Response(
-     *      response=404,
-     *      description="not found"
-     *   ),
-     *  )
-     */
-    public function index() {
-        //liste de tous les élements
-        $person = People::with(['vehicles:url','species', 'homeworld'])->get();
-             return response()->json($person);
-           }
-        
-           public function store(Request $request) {
-             echo 'store';
-           }
-           public function show($id) {
-               $person = People::with(['vehicles:url','species', 'homeworld'])->find($id);
-             //
-               return response()->json($person);
-           }
+   *      summary="Get List Of People",
+   *      description="Returns all people and associated vehicle, specie.",
+   *      @OA\Response(
+   *          response=200,
+   *          description="Successful operation",
+   *          @OA\MediaType(
+   *           mediaType="application/json",
+   *      )
+   *      ),
+   *      @OA\Response(
+   *          response=401,
+   *          description="Unauthenticated",
+   *      ),
+   *      @OA\Response(
+   *          response=403,
+   *          description="Forbidden"
+   *      ),
+   * @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *   ),
+   * @OA\Response(
+   *      response=404,
+   *      description="not found"
+   *   ),
+   *  )
+   */
+  public function index()
+  {
+    //liste de tous les élements
+    $person = People::with(['vehicles:url', 'species', 'homeworld'])->get();
+    return response()->json($person);
+  }
 
-           public function edit($id) {
-              echo 'edit';
-           }
-           public function update(Request $request, $id) {
-                echo 'update';
-           }
-           public function destroy($id) {
-              echo 'destroy';
-           }
+  public function store(Request $request)
+  {
+    echo 'store';
+  }
 
+  /**
+   * @OA\Get(
+   *      path="/people/{id}",
+   *      operationId="getAPerson",
+   *      tags={"Tests"},
+
+   *      summary="Get a person",
+   *      description="Returns a person and associated vehicle, specie.",
+   *      @OA\Parameter(
+   *        name="country",
+   *        in="path",
+   *        required=true,
+   *        @OA\Schema(
+   *           type="string"
+   *           )
+   *        ),
+   * @OA\Response(
+   *          response=200,
+   *          description="Successful operation",
+   *          @OA\MediaType(
+   *           mediaType="application/json",
+   *        )
+   *      ),
+   *      @OA\Response(
+   *          response=401,
+   *          description="Unauthenticated",
+   *      ),
+   *      @OA\Response(
+   *          response=403,
+   *          description="Forbidden"
+   *      ),
+   * @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *   ),
+   * @OA\Response(
+   *      response=404,
+   *      description="not found"
+   *   ),
+   *  )
+   */
+  public function show($id)
+  {
+    $person = People::with(['vehicles:url', 'species', 'homeworld'])->find($id);
+    //
+    return response()->json($person);
+  }
+
+  public function edit($id)
+  {
+    echo 'edit';
+  }
+  public function update(Request $request, $id)
+  {
+    echo 'update';
+  }
+  public function destroy($id)
+  {
+    echo 'destroy';
+  }
 }
