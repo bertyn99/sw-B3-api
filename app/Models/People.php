687b96cb3a16d4e55c36dd4474b585732cdd8a1f
Bertyn Boulikou <bboulikou@gmail.com>
2021-10-19 13:10:20 +0200

    ajout model swagger

--- app/Models/People.php
+++ app/Models/People.php
@@ -4,7 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ *
+ * @OA\Schema(
+ * 
+ * @OA\Xml(name="People"),
+ * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="name", type="string", description="people name"),
+ * @OA\Property(property="birth_year", type="string", description="People birth year"),
+ * @OA\Property(property="eye_color", type="string", description="People eye color"),
+ * @OA\Property(property="hair_color", type="string", description="People hair color"),
+ * @OA\Property(property="skin_color", type="string", description="People skin color"),
+ * @OA\Property(property="height", type="string", description="People height"),
+ * @OA\Property(property="mass", type="string", description="People mass"),
+ * @OA\Property(property="gender", type="string", description="People gender"),
+ * @OA\Property(property="hright", type="string", description="People url"),
+ * @OA\Property(property="created_at", type="string", format="date-time", description="Initial creation timestamp", readOnly="true"),
+ * @OA\Property(property="updated_at", type="string", format="date-time", description="Last update timestamp", readOnly="true"),
+ * @OA\Property(property="deleted_at", type="string", format="date-time", description="Soft delete timestamp", readOnly="true")
+ * )
+ *
+ * Class People
+ *
+ */
 class People extends Model
 {
     use HasFactory;
--- app/Models/Planet.php
+++ app/Models/Planet.php
@@ -5,6 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ *
+ * @OA\Schema(
+ * 
+ * @OA\Xml(name="Planet"),
+ * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="name", type="string", description="people name"),
+ * @OA\Property(property="diameter", type="string", description="People birth year"),
+ * @OA\Property(property="rotation_period", type="string", description="People eye color"),
+ * @OA\Property(property="orbital_period", type="string", description="People hair color"),
+ * @OA\Property(property="gravity", type="string", description="People skin color"),
+ * @OA\Property(property="population", type="string", description="People height"),
+ * @OA\Property(property="climate", type="string", description="Planet climate"),
+ * @OA\Property(property="terrain", type="string", description="Planet terrain"),
+ * @OA\Property(property="surface_water", type="string", description="Planet terrain"),
+ * @OA\Property(property="url", type="string", description="People url"),
+ * @OA\Property(property="created_at", type="string", format="date-time", description="Initial creation timestamp", readOnly="true"),
+ * @OA\Property(property="updated_at", type="string", format="date-time", description="Last update timestamp", readOnly="true"),
+ * @OA\Property(property="deleted_at", type="string", format="date-time", description="Soft delete timestamp", readOnly="true")
+ * )
+ *
+ * Class Planet
+ *
+ */
 class Planet extends Model
 {
     use HasFactory;
